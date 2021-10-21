55522654ed9915153b490a242b8d11bcde8af694
Themba <thembelanimahlangu@outlook.com>
2021-10-21 10:57:02 +0200

    update projname

--- cookiecutter/zipfile.py
+++ cookiecutter/zipfile.py
@@ -69,7 +69,7 @@ def unzip(zip_uri, is_url, clone_to_dir='.', no_input=False, password=None):
            # )
 
         # Construct the final target directory
-        project_name = first_filename[:-1]
+        project_name = zip_uri.strip('.zip').strip('./')
         # changed base path 
         unzip_base = '.'
         unzip_path = os.path.join(unzip_base, project_name)
